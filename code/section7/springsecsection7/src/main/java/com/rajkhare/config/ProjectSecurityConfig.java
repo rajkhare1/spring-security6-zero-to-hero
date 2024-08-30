8e56eaab79f48f9ef701d138f7fd54547f26cffa
rajkhare1 <raj.khare.java@gmail.com>
2024-08-30 15:05:14 +0530

    update:30-Aug-2024

---
+++ code/section7/springsecsection7/src/main/java/com/rajkhare/config/ProjectSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.rajkhare.config;
+
+import com.rajkhare.exceptionhandling.CustomAccessDeniedHandler;
+import com.rajkhare.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("!prod")
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
+        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())//Only HTTP
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
+                .requestMatchers("/contact","/notices","/error","/register","/invalidSession").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+        return http.build();
+    }
+
+   /* @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+            return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * From Spring Securtiy 6.3
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
