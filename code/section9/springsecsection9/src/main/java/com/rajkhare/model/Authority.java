78875ffe4d100f493fe6df252a3ad760801a5ece
rajkhare1 <raj.khare.java@gmail.com>
2024-09-30 21:12:32 +0530

    update:30-Sep-2024

---
+++ code/section9/springsecsection9/src/main/java/com/rajkhare/model/Authority.java
@@ -0,0 +1,12 @@
+package com.rajkhare.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "authorities")
+public class Authority {
+}
