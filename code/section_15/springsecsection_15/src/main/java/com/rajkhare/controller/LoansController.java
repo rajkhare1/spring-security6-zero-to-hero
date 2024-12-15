9b9e807ea885ecfd3f177d03945d7d3cd002e5a5
Raj Khare <rajkhare@Rajs-MacBook-Air.local>
2024-12-15 20:43:46 +0530

    updateM0:15-12-2024

--- code/section_15/springsecsection_15/src/main/java/com/rajkhare/controller/LoansController.java
+++ code/section_15/springsecsection_15/src/main/java/com/rajkhare/controller/LoansController.java
@@ -1,6 +1,8 @@
 package com.rajkhare.controller;
 
+import com.rajkhare.model.Customer;
 import com.rajkhare.model.Loans;
+import com.rajkhare.repository.CustomerRepository;
 import com.rajkhare.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -9,22 +11,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class LoansController {
 
     private final LoanRepository loanRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/myLoans")
     @PostAuthorize(value = "hasRole('USER')")
-    public List<Loans> getLoanDetails(@RequestParam long id) {
-        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
-        if (loans != null) {
-            return loans;
+    public List<Loans> getLoanDetails(@RequestParam String  email) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(email);
+        if(optionalCustomer.isPresent()) {
+            List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(optionalCustomer.get().getId());
+            if (loans != null) {
+                return loans;
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
+
     }
 
 }
